bdc58209af958893bf2103e07c7f08f43ba31e37
Michael Golenia <michael@wahres-leben.de>
2026-02-06 14:46:29 +0100

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,28 @@
 import { Component } from "react"
 import GameBoard from "./Components/GameBoard/GameBoard"
+import Game from './Models/Game'
 import Player from "./Models/Player"
 import './App.css'
 
 type AppState = {
-  gameFields: Player[][]
+    gameFields: Player[][]
 }
 class App extends Component<{}, AppState> {
-  players = [
-    new Player("Kreuz","X"), 
-    new Player("Kreis","O")
-  ]
-
-  currentPlayerNumber: number = 0
-  round: number = 0
+  game = new Game()
 
   constructor(props: {}) {
     super(props)
 
-    const gameFields: Player[][] = Array(4).fill(null).map(() => Array(4).fill(null))
-
     this.state = {
-      gameFields: gameFields
+      gameFields: this.game.gameFields
     }
 
     this.handleMove = this.handleMove.bind(this)
   }
 
   handleMove(row: number, col: number) {
-    const newFields = this.state.gameFields.map(r => [...r]);
-
-    if (newFields[row][col] == null) {
-      // Update GameField
-      newFields[row][col] = this.players[this.currentPlayerNumber]
-      this.setState({ gameFields: newFields })
-
-      this.checkLost(row,col);
-
-      // switch player
-      this.currentPlayerNumber = (this.currentPlayerNumber + 1) % 2
-
-      this.round++;
-    }
-  }
-
-  checkLost(row: number, col: number) {
-    const gameField = this.state.gameFields
-    let countSymbols = 0
-
-    if(this.round >= 15) {
-      alert("Unentschieden")
-    }
-
-    if (this.round > 0 /* 4 */) {
-      if (row > 1) {
-        countSymbols = 0
-        for(let c = 1; c < 3; c++) {
-          const checkField = gameField[row-c][col]
-
-          if(checkField != null && checkField.symbol == this.players[this.currentPlayerNumber].symbol) {
-            countSymbols++;
-          }
-        }
-
-        alert(countSymbols)
-
-        if(countSymbols > 2) {
-          alert("Spieler " + this.players[this.currentPlayerNumber].name + " hat verloren")
-        }
-      }
-    }
-
- 
+    this.game.makeMove(row, col)
+    this.setState({ gameFields: [...this.game.gameFields.map(row => [...row])] })
   }
 
   render() {
--- src/Models/Game.ts
+++ src/Models/Game.ts
@@ -1,5 +1,23 @@
-class Game {
+import Player from "./Player"
 
+class Game {
+  players = [
+    new Player("Kreuz","X"), 
+    new Player("Kreis","O")
+  ]
+
+  currentPlayerNumber: number = 0
+  round: number = 0
+
+  gameFields: Player[][] = Array(4).fill(null).map(() => Array(4).fill(null))
+
+  makeMove(row: number, col: number) {
+    if (this.gameFields[row][col] == null) {
+      this.gameFields[row][col] = this.players[this.currentPlayerNumber]
+      this.round++;
+      this.currentPlayerNumber = (this.currentPlayerNumber + 1) % 2
+    }
+  }
 }
 
 export default Game
