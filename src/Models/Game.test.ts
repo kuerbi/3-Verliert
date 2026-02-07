7b8106856158f838ed187175ee1559ae958ec63e
Michael Golenia <michael@wahres-leben.de>
2026-02-07 15:11:25 +0100

    Add more tests

--- src/Models/Game.test.ts
+++ src/Models/Game.test.ts
@@ -1,24 +1,38 @@
-import { expect, test, describe, it } from 'vitest'
+import { expect, test, describe, it, beforeEach } from 'vitest'
 import Game from './Game'
 
 describe("Game", () => {
+    let game: Game = new Game()
+
+
+    beforeEach(() => {
+      game = new Game()
+      game.gameField = Array(4).fill(null).map(() => Array(4).fill(null))
+    })
+
     it("player has lost in right direction", () => {
-      let game: Game = new Game()
         for(let c = 0; c < 2; c++) {
           for(let r = 0; r < game.gameField.length; r++) {
             for(let v = 0; v < 3; v++) {
               game.gameField[r][c+v] = "X"
             }
             expect(game.checkPlayerHasLost(game.players[0], r, c)).toBe(true)
-            game.gameField = [
-              [null,null,null,null],
-              [null,null,null,null],
-              [null,null,null,null],
-              [null,null,null,null],
-            ]
-          }
+            game.gameField = Array(4).fill(null).map(() => Array(4).fill(null))
 
+          }
         }      
+    })
+
+    it("player has lost in left direction", () => {
+      for(let c = 3; c > 1; c--) {
+        for(let r = 0; r < game.gameField.length; r++) {
+          for(let v = 0; v < 3; v++) {
+            game.gameField[r][c-v] = "X"
+          }
+          expect(game.checkPlayerHasLost(game.players[0], r, c)).toBe(true)
+          game.gameField = Array(4).fill(null).map(() => Array(4).fill(null))
+        }
+      }      
     })
 })
 
